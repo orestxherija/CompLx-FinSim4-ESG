8b936ee23ce45e62d4c85ff9dcda2b09ee959073
Orest Xherija <xherija.orest@gmail.com>
2022-06-14 22:01:28 -0500

    train, predict scripts complete

--- run_predict.sh
+++ run_predict.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-HF_MODEL="checkpoints/beto-base-cased-20220418202326"
+
+HF_MODEL="checkpoints/deberta-v3-large-finetuned-finsim4-esg"
 MODEL_NAME=$(echo $HF_MODEL | rev | cut -d/ -f1 | rev)
 
 python hf_predict.py \
   --model_name_or_path $HF_MODEL \
-  --test_file data/test_new.csv \
+  --test_file data/processed/test.csv \
   --do_predict \
   --max_seq_length 128 \
   --per_device_eval_batch_size 256 \
--- run_train.sh
+++ run_train.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-HF_MODEL="espejelomar/beto-base-cased"
+
+HF_MODEL="microsoft/deberta-v3-large"
 MODEL_NAME=$(echo $HF_MODEL | rev | cut -d/ -f1 | rev)
-TIMESTAMP=$(date +"%Y%m%d%H%M%S")
+EXTENSION="finetuned-finsim4-esg"
 
 python hf_train.py \
   --model_name_or_path $HF_MODEL \
   --do_train \
   --do_eval \
-  --train_file train_new.csv \
-  --validation_file dev_new.csv \
-  --max_seq_length 128 \
-  --per_device_train_batch_size 64 \
+  --train_file data/processed/train.csv \
+  --validation_file data/processed/dev.csv \
+  --max_seq_length 64 \
+  --per_device_train_batch_size 8 \
   --learning_rate 2e-5 \
   --num_train_epochs 25 \
-  --output_dir checkpoints/${MODEL_NAME}-${TIMESTAMP}/ \
+  --output_dir checkpoints/${MODEL_NAME}-${EXTENSION}/ \
   --cache_dir .cache/ \
   --overwrite_output_dir \
   --pad_to_max_length \
@@ -21,7 +22,7 @@ python hf_train.py \
   --data_seed 2022 \
   --fp16 \
   --load_best_model_at_end \
-  --metric_for_best_model eval_f1 \
+  --metric_for_best_model eval_accuracy \
   --evaluation_strategy steps \
   --group_by_length \
   --label_smoothing_factor 0.1 \
